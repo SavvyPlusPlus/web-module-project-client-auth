d6d010eda81d9cccfac8df3c5439a1bff331dd12
SavvyPlusPlus <info@savvydc.com>
2021-07-11 21:10:43 -0400

    MVP met

--- friends/src/App.js
+++ friends/src/App.js
@@ -4,48 +4,28 @@ import Login from './components/Login';
 import FriendList from './components/FriendList';
 import PrivateRoute from './components/PrivateRoute';
 import FriendForm from './components/FriendForm';
+import React, { Fragment } from 'react'
+import LandingPage from './components/LandingPage';
 
 function App() {
 
-  const logout = () => {
-    localStorage.removeItem("token");
-    console.log("Logged out!");
-  };
+
 
   const isAuth = localStorage.getItem("token");
 
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <p>Module Project - Authentication</p>
-
-          <ul>
-            <li>
-
-              <Link to="/login">login</Link>
-            </li>
-            <li>
-              <Link to="/friends">Friends</Link>
-            </li>
-            <li>
-              <Link onClick={logout}>Logout</Link>
-            </li>
-            <li>
-             <Link to="/friends/form">Friends Form</Link>
-            </li>
-            <li>
-              <Link to="/protected"> Protected Page</Link>
-            </li>
-          </ul>
+      
           <Switch>
+            <Route exact path="/" component={LandingPage}/>
             <PrivateRoute exact path="/friends/form" component={FriendForm} />
             <PrivateRoute exact path="/friends" component={FriendList} />
             <Route path="/login" component={Login} />
             <Route component={Login} />
           </Switch>
 
-        </header>
+    
       </div>
     </Router>
   );
--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import FriendForm from './FriendForm';
 import axiosWithAuth from './helpers/axiosWithAuth';
 
 class FriendList extends React.Component {
@@ -27,16 +29,38 @@ class FriendList extends React.Component {
 
     render() {
         return (
-            <div>
-                {this.state.friends.map(friend => (
-                    <div key={friend.id}>
-                        <p>{friend.name}</p>
-                        <p>{friend.age}</p>
-                        <p>{friend.email}</p>
-                        <p>---------------</p>
+            
+            <div className="App ">
+                 <FriendForm/>
+
+
+
+
+
+
+
+            {this.state.friends.map(friend => (
+
+                <div className="shadow-lg rounded-2xl bg-white dark:bg-gray-800 p-4">
+                    <div clasName="flex-col  flex justify-center items-center">
+                        <div className="flex-shrink-0">
+                            <img className="mx-auto object-cover rounded-full h-16 w-16" src="https://uploads-ssl.webflow.com/6010580d08831070b0339b25/60eb8d32a4eb8542a3369d99_pngwing.com%20(1).png" alt="ProfilePix" />
+                        </div>
+                        <div key={friend.id} className="mt-2 text-center flex flex-col">
+                            <span class="text-gray-600 dark:text-white text-lg font-medium">
+                            {friend.name}</span>
+                            <span class="text-gray-400 text-xs">
+                            {friend.age}</span>
+                            <span class="text-gray-400 text-xs">
+                            {friend.email}</span>
+                            <span class="text-gray-400 text-xs">
+                            __________</span>
+
+                        </div>
                     </div>
-                )
-                )}
+            
+                </div>
+            ))} 
 
             </div>
 
